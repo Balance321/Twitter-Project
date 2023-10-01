216eb59ffd5c90cf21736ee02b19098729a96964
ruhsul <145957993+ruhsul@users.noreply.github.com>
2023-10-01 16:00:17 -0700

    Create twitter_api.py

---
+++ twitter_api.py
@@ -0,0 +1,11 @@
+import keys_test as keys
+import tweepy
+
+def generateTweet(tweet): 
+    client = tweepy.Client(consumer_key=keys.api_key_final,
+                       consumer_secret=keys.api_secret_final,
+                       access_token=keys.access_token_final,
+                       access_token_secret=keys.access_token_secret_final)
+
+    client.create_tweet(text=tweet)
+    print(tweet) 
