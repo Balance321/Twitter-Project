2898cb8321e1663a65265f613545e998b10bf326
ruhsul <145957993+ruhsul@users.noreply.github.com>
2023-10-04 20:58:44 -0700

    Update twitter_api.py

--- twitter_api.py
+++ twitter_api.py
@@ -2,6 +2,7 @@ import keys_test as keys
 import tweepy
 
 def generateTweet(tweet): 
+    """
     client = tweepy.Client(consumer_key        = keys.api_key_final,
                            consumer_secret     = keys.api_secret_final,
                            access_token        = keys.access_token_final,
@@ -9,3 +10,10 @@ def generateTweet(tweet):
 
     client.create_tweet(text = tweet)
     # print(tweet) 
+    """
+
+    auth = tweepy.OAuthHandler(keys.api_key_final, keys.api_secret_final)
+    auth.set_access_token(keys.access_token_final, keys.access_token_secret_final)
+
+    api = tweepy.API(auth)
+    api.update_status(tweet)
