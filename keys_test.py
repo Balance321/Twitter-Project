49b4dcd018ae1577a65363f9ea1ce491810ab566
Balance321 <93746122+Balance321@users.noreply.github.com>
2023-10-05 22:26:30 -0700

    Update keys_test.py

--- keys_test.py
+++ keys_test.py
@@ -1,21 +1,24 @@
 from turtle import clear
 from collections import OrderedDict
 
-# Twitter
-api_key = 'xjP85DKxKG95H8O4Q2tBxx5lP'
-api_secret = '7vYbolTHKpCRdbIc6zItRuBbbxp7UF90l701FDysExn2R0nP4w'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAANaNqAEAAAAAA3JBRU6hqnWlOFqUb7Jyb5n4t1g%3D3fzKDwpe94DYkOtcjMKFGZMKrxiEHAoMBmZV9jkJu27U8R5e7W'
-access_token = '1706180546878214144-HD7oCzatyJas7yxmGXxZSKP34jbs44'
-access_token_secret = 'qDoI3b09duvutIZdDw9mDWizeoaR6cJtp5Db2Nso2mvWD'
 
-api_key_final = 'LrGML5OGXJhjRMONqkoW9UMYp'
-api_secret_final = '8zojfEQKlHLp27wq6QtJpsk1XBKjQy65EfW8o48CU7czpvSonB'
-bearer_token_final = 'AAAAAAAAAAAAAAAAAAAAAJX5qAEAAAAAFnAg%2FJAa1U63XG%2BZK%2F2FVaKYUPU%3DLNPsqw0DiObjw7FojJDW1CKqDNbtkRrjClqMQIIqwxKEfu0HLx'
-access_token_final = '1708287959743815680-onMfErVzHDTxN0hIzSRRzXX0YQ8G9w'
-access_token_secret_final = 'tgdOpsmHxugWPEZcVcR73J4I4j395DChhpaTOP664kzqN'
+#Add in your own api keys below 
+
+# Twitter
+api_key = ''
+api_secret = ''
+bearer_token = ''
+access_token = ''
+access_token_secret = ''
+
+api_key_final = ''
+api_secret_final = ''
+bearer_token_final = ''
+access_token_final = ''
+access_token_secret_final = ''
 
 # MAL
-client_id = 'adda09f6a4fc2b591f6e65028d31f458'
-client_secret = 'f359587358e78986a66d2645cf18a42fade7748936d5cb27a0016c04c7403453'
+client_id = ''
+client_secret = ''
 
 # Schedule tweets for end of the week
